e6015bca2a23d02ec3c97b317363c38f660c543b
Marcin Żak <marcinzak1992@gmail.com>
2020-05-02 12:18:45 +0200

    Add project files.

---
+++ ListBox/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> lista = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                listBox1.Items.Add(String.Format("Opcja {0}", i));
+                listBox2.Items.Add(String.Format("Opcja {0}", (char)(i + (int)'A')));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (listBox1.SelectedIndices.Count!=0)
+            { 
+                listBox2.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            while (listBox2.SelectedItems.Count != 0)
+            {
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            }
+        }
+    }
+}
